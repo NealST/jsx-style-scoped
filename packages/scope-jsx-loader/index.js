98c4157c47f731e9ff0577068ea66600c1cf9242
mozheng.sh <mozheng.sh@alibaba-inc.com>
2020-07-28 19:14:07 +0800

    improve jsx scope loader

--- packages/scope-jsx-loader/index.js
+++ packages/scope-jsx-loader/index.js
@@ -8,20 +8,19 @@ const path = require("path");
 
 const computedHash = {};
 
-const computeHash = (filePath) => {
-  if (computedHash[filePath]) {
-    return computedHash[filePath]
+const computeHash = (pkgName) => {
+  if (computedHash[pkgName]) {
+    return computedHash[pkgName]
   }
-
-  const relative = path.relative(process.cwd(), filePath);
-  const hash = md5(relative).substr(0, 8);
-  computedHash[filePath] = hash;
+  const hash = md5(pkgName).substr(0, 8);
+  computedHash[pkgName] = hash;
   return hash;
 }
 
 module.exports = function(source) {
-  const filePath = this.resourcePath;
-  const hash = computeHash(filePath);
+  const cwd = process.cwd();
+  const pkgName = require(path.join(cwd, 'package.json')).name;
+  const hash = computeHash(pkgName);
   let classnames = [];
   
   return source.replace(classNameReg, (match) => {
